b5137ddbeaaf3a655f8847fa7e1342a84cd180fb
Alexanyer Naranjo <alexanyernaranjo@gmail.com>
2024-07-18 08:38:09 -0400

    fix: Footer Section

--- src/components/Home/FooterSection.vue
+++ src/components/Home/FooterSection.vue
@@ -2,7 +2,7 @@
     <v-footer class="d-flex flex-column bg-dark-blue">
         <div class="py-12 text-caption text-center text-white">
             <p>&copy; {{ new Date().getFullYear() }} Universidad Central de Venezuela (UCV). Todos los Derechos Reservados.</p>
-            <p>Desarrollador por Alexanyer Naranjo.</p>
+            <p>Desarrollado por Alexanyer Naranjo.</p>
         </div>
     </v-footer>
 </template>
